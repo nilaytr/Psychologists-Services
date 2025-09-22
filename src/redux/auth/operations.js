2282c078f3725d952804d8686f82fb89977716f2
Nilay Türker <nilaytr89@gmail.com>
2025-09-22 23:37:32 +0300

    auth slice

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,3 +1,10 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
+//import { createAsyncThunk } from "@reduxjs/toolkit";
+//import axios from "axios";
+//import {
+//    createUserWithEmailAndPassword,
+//    signInWithEmailAndPassword,
+//    updateProfile,
+//    signOut,
+//    onAuthStateChanged,
+//} from "firebase/auth";
+//import { auth } from "../../firebase";
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,3 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { loginUser, refreshUser, registerUser, logoutUser } from "./operations";
+import { loginUser, registerUser, logoutUser } from "./operations";
 
+const handlePending = (state) => {
+    state.loading = true;
+    state.error = null;
+};
+
+const handleRejected = (state, action) => {
+    state.loading = false;
+    state.error = action.payload || action.error?.message;
+};
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        user: null,
+        loading: false,
+        error: null,
+    },
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload;
+        },
+        clearUser: (state) => {
+            state.user = null;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(registerUser.pending, handlePending)
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload;
+            })
+            .addCase(registerUser.rejected, handleRejected)
+
+            .addCase(loginUser.pending, handlePending)
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload;
+            })
+            .addCase(loginUser.rejected, handleRejected)
+        
+            .addCase(logoutUser.pending, handlePending)
+            .addCase(logoutUser.fulfilled, (state) => {
+                state.loading = false;
+                state.user = null;
+            })
+            .addCase(logoutUser.rejected, handleRejected)
+    },
+});
+
+export default authSlice.reducer;
