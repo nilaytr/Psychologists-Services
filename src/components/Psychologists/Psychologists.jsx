45039cff400adb54185ea212dfb7eb3336932e1c
Nilay Türker <nilaytr89@gmail.com>
2025-09-28 23:43:10 +0300

    psychologists page updates

--- src/components/Psychologists/Psychologists.jsx
+++ src/components/Psychologists/Psychologists.jsx
@@ -1,4 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectPsychologists, selectCurrentPage, selectPerPage, selectFilter } from "../../redux/psychologists/selectors";
+import { setPage, resetPage } from "../../redux/psychologists/slice";
 import Filter from "../Filter/Filter";
 import PsychologistItem from "../PsychologistItem/PsychologistItem";
+
+const Psychologists = () => {
+    const dispatch = useDispatch();
+    const psychologists = useSelector(selectPsychologists);
+    const currentPage = useSelector(selectCurrentPage);
+    const perPage = useSelector(selectPerPage);
+    const filter = useSelector(selectFilter);
+}
 
 
